4d56eb2b572d1f8c31d235414f042e6db379800a
TJ <i@voox.cc>
2020-04-29 17:57:34 +0800

    Add UI

---
+++ src/main/java/cc/voox/graphql/ui/AltairProperties.java
@@ -0,0 +1,61 @@
+//package cc.voox.graphql.ui;
+//
+//public class AltairProperties {
+//
+//    private boolean enabled = true;
+//    private Endpoint endpoint = new Endpoint();
+//    private Static STATIC = new Static();
+//    private Cdn cdn = new Cdn();
+////    private String pageTitle = "Altair UI";
+////    private String mapping = "/app/altair";
+//
+//
+//
+//    public boolean isEnabled() {
+//        return enabled;
+//    }
+//
+//    public void setEnabled(boolean enabled) {
+//        this.enabled = enabled;
+//    }
+//
+//    public Endpoint getEndpoint() {
+//        return endpoint;
+//    }
+//
+//    public void setEndpoint(Endpoint endpoint) {
+//        this.endpoint = endpoint;
+//    }
+//
+//    public Static getSTATIC() {
+//        return STATIC;
+//    }
+//
+//    public void setSTATIC(Static STATIC) {
+//        this.STATIC = STATIC;
+//    }
+//
+//    public Cdn getCdn() {
+//        return cdn;
+//    }
+//
+//    public void setCdn(Cdn cdn) {
+//        this.cdn = cdn;
+//    }
+//
+//    public String getPageTitle() {
+//        return pageTitle;
+//    }
+//
+//    public void setPageTitle(String pageTitle) {
+//        this.pageTitle = pageTitle;
+//    }
+//
+//    public String getMapping() {
+//        return mapping;
+//    }
+//
+//    public void setMapping(String mapping) {
+//        this.mapping = mapping;
+//    }
+//}
