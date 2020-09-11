5d002a4f4f36a05c33953d004de3e7d17db3a6e7
TJ <i@voox.cc>
2020-09-11 16:33:15 +0800

    code improvement get root entity

--- src/main/java/cc/voox/graphql/annotation/QueryField.java
+++ src/main/java/cc/voox/graphql/annotation/QueryField.java
@@ -8,6 +8,20 @@ import java.lang.annotation.Target;
 @Target({ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface QueryField {
+    /**
+     * field name
+     */
     String value();
+
+    /**
+     * Will get field value from root entity if source is true
+     */
     boolean source() default false;
+
+    /**
+     * Will get root entity if root is true
+     * if source is true, root is disable
+     */
+    boolean root() default false;
+
 }
